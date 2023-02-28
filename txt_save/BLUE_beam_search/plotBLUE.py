4abcde7fe35af6890186de4d1e2140d3a38c5107
leonardodipre <leonardo.dipre@gmail.com>
2023-02-28 12:29:23 +0100

    BLEU not BLUE

--- txt_save/BLUE_beam_search/plotBLUE.py
+++ txt_save/BLUE_beam_search/plotBLUE.py
@@ -5,9 +5,9 @@ import numpy
 
 # Carica il file CSV in un DataFrame di Pandas
 
-#df = pd.read_csv(r'D:\Leonardo\UAV_VQG\VQG_UAV\txt_save\BLUE_beam_search\BLUE_beam_search.csv')
+df = pd.read_csv(r'D:\Leonardo\UAV_VQG\VQG_UAV\txt_save\BLUE_beam_search\BLUE_beam_search.csv')
 
-df = pd.read_csv(r'D:\Leonardo\UAV_VQG\VQG_UAV\txt_save\BLUE_multimodal\BLUE_UAV_multimodal.txt')
+#df = pd.read_csv(r'D:\Leonardo\UAV_VQG\VQG_UAV\txt_save\BLUE_multimodal\BLUE_UAV_multimodal.csv')
 
 #df = pd.read_csv(r'D:\Leonardo\UAV_VQG\VQG_UAV\txt_save\BLUE_gredy\BLUE_UAV.csv')
 
@@ -21,11 +21,10 @@ BLEU4 = df['BLEU4']
 
 
 
-plt.plot(epochs.astype(str), BLEU1, label="BLUE1")
-
-plt.plot(epochs.astype(str), BLEU2, label="BLUE2")
-plt.plot(epochs.astype(str), BLEU3, label="BLUE3")
-plt.plot(epochs.astype(str), BLEU4, label="BLUE4")
+plt.plot(epochs.astype(str), BLEU1, label="BLEU1")
+plt.plot(epochs.astype(str), BLEU2, label="BLEU2")
+plt.plot(epochs.astype(str), BLEU3, label="BLEU3")
+plt.plot(epochs.astype(str), BLEU4, label="BLEU4")
 
 plt.legend()
 
@@ -36,4 +35,4 @@ plt.xlabel('model')
 
 # Mostra il grafico
 
-plt.savefig('BLEU_UAV_multinomial_total.png')
+plt.savefig('BLEU_UAV_beam_search_total.png')
